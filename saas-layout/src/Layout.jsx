2b4f4abf4cd2b7bc5e00bc199c46b51c83d66c16
Favour Egwele <favoregwele@gmail.com>
2026-08-21 15:30:41 +0200

    First addition: header

---
+++ saas-layout/src/Layout.jsx
@@ -0,0 +1,52 @@
+function Layout() {
+    return ( 
+        <div className="layout">
+            <section className="side-bar">
+                <h1>EliteCo</h1>    
+                Sidebar
+            </section>
+
+            <section className="body-cont">
+                
+                <div className="header">
+                
+                    <div className="header-one">
+
+                        <div className="search-bar">
+                            <i className="bi bi-search"></i>
+                            <p>search anything</p>
+                            <i className="bi bi-record-circle"></i>
+                        </div>
+
+                    </div>
+
+                    <div className="header-two">
+                        <i className="bi bi-circle-half"></i>
+
+                        <div className="with-notif">    
+                            <i className="bi bi-clock"></i> 
+                            <span className="notif notif-one">
+                                <p>7</p>
+                            </span>
+                        </div>
+                        
+                        <div className="with-notif">
+                            <i class="bi bi-bell"></i>
+                            <span className="notif notif-two">
+                                <p>14</p>
+                            </span>
+                        </div>
+
+                        <img src="/images/Me.jpeg" alt="Me" />
+                    </div>
+                
+                </div>
+                
+                <span>Body</span>
+            
+            </section>
+        </div>
+    );
+}
+
+export default Layout;
